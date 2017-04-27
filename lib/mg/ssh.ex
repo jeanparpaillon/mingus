fd7d3ac88f8996603bd2761b33a983fa95299748
Jean Parpaillon <jean.parpaillon@free.fr>
2017-04-27 10:31:32 +0200

    Support rsa/dsa/ecdsa keys

--- lib/mg/ssh.ex
+++ lib/mg/ssh.ex
@@ -1,12 +1,13 @@
 defmodule Mg.SSH do
   import Supervisor.Spec
+  require Record
   require Logger
   alias Mg.Store
   alias Mg.Utils
   alias Mg.SSH.Keys
 
   def start_link(opts) do
-    host_keys = ensure_host_keys(Keyword.get(opts, :gen_host_key, true))
+    host_keys = Keys.ensure_host_keys(Keyword.get(opts, :gen_host_key, true))
     system_dir = Path.join(:code.priv_dir(:mingus), "keys")
     ssh_opts = [
       id_string: 'Mingus Orchestrator',
@@ -24,37 +25,5 @@ defmodule Mg.SSH do
     end)
 
     Supervisor.start_link(listeners, strategy: :one_for_one)
-  end
-
-  ###
-  ### Private
-  ###
-  defp ensure_host_keys(generate) do
-    dir = Path.join(:code.priv_dir(:mingus), "keys")
-    if Path.wildcard("#{dir}/ssh_host_*_key") == [] and generate do
-      generate_host_keys(dir)
-    end
-
-    Path.wildcard("#{dir}/ssh_host_*_key") |> Enum.reduce(%{}, fn path, acc ->
-      bin = File.read!(path)
-      :public_key.pem_decode(bin) |> Enum.reduce(acc, fn
-        ({:"RSAPrivateKey", _, :not_encrypted}=key, acc2) -> Map.put(acc2, :"ssh-rsa", key)
-        ({:"DSAPrivateKey", _, :not_encrypted}=key, acc2) -> Map.put(acc2, :"ssh-dss", key)
-        # Don't know how to treat ec keys: ecdsa-sha2-nistp{256,384,521} ???
-        ({:"ECPrivateKey", _, :not_encrypted}=key, acc2) -> acc2
-        (key, acc2) -> acc2
-      end)
-    end)
-  end
-
-  defp generate_host_keys(dir) do
-    ["rsa", "dsa", "ecdsa", "ed25519"] |> Enum.each(fn type ->
-      path = Path.join([dir, "ssh_host_#{type}_key"])
-      if not File.exists?(path) do
-        Logger.info("Generating SSH host key pair (#{type})...")
-        comment = Application.get_env(:mingus, :id, "mingus") <> "@mingus"
-        System.cmd("ssh-keygen", ["-t", type, "-C", comment, "-f", path, "-q"], stderr_to_stdout: true)
-      end
-    end)
   end
 end
