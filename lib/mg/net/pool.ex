99e6c58f688ee90af6e0107df53d4b3e9e268567
Jean Parpaillon <jean.parpaillon@free.fr>
2017-05-09 11:47:14 +0200

    Net: Fix reservation of block in partially reserved ones

--- lib/mg/net/pool.ex
+++ lib/mg/net/pool.ex
@@ -3,12 +3,32 @@ defmodule Mg.Net.Pool do
   require Logger
   alias Mg.Net.Pool
 
-  defstruct ip: nil, mask: 0, free: true, low: nil, high: nil, type: :inet, netmask: 0
+  defstruct ip: nil, mask: 0, status: :free, low: nil, high: nil, type: :inet, netmask: 0
+
+  @type t :: %Pool{}
+  @type policy :: :low | :high | :random
+  @type mask :: :unique | integer
+  @type status :: :free | :partial | :lease | :reserved
 
   @doc """
   Create new pool from address + mask or CIDR string
 
   ## Examples
+
+      iex> Mg.Net.Pool.new({192, 168, 100, 231}, 24)
+      %Mg.Net.Pool{free: true, high: nil, ip: <<192, 168, 100, 0>>, low: nil, mask: 24, netmask: 24, type: :inet}
+  """
+  @spec new(:inet.ip_address, number) :: Pool.t
+  def new({_, _, _, _}=addr, mask), do: new(addr, mask, :inet)
+  def new({_, _, _, _, _, _, _, _}=addr, mask), do: new(addr, mask, :inet6)
+
+  @doc """
+  Create new pool from address + mask or CIDR string
+
+  ## Examples
+
+      iex> Mg.Net.Pool.new({{192, 168, 100, 231}, 24})
+      %Mg.Net.Pool{free: true, high: nil, ip: <<192, 168, 100, 0>>, low: nil, mask: 24, netmask: 24, type: :inet}
 
       iex> Mg.Net.Pool.new("192.168.100.231/24")
       %Mg.Net.Pool{free: true, high: nil, ip: <<192, 168, 100, 0>>, low: nil, mask: 24, netmask: 24, type: :inet}
@@ -19,9 +39,9 @@ defmodule Mg.Net.Pool do
       iex> Mg.Net.Pool.new("5.39.110.160/27")
       %Mg.Net.Pool{free: true, high: nil, ip: <<5, 39, 110, 160>>, low: nil, mask: 27, netmask: 27, type: :inet}
   """
-  def new({_, _, _, _}=addr, mask), do: new(addr, mask, :inet)
-  def new({_, _, _, _, _, _, _, _}=addr, mask), do: new(addr, mask, :inet6)
-
+  @spec new({:inet.ip_address, number} | String.t) :: Pool.t
+  def new({{_, _, _, _}=addr, mask}), do: new(addr, mask, :inet)
+  def new({{_, _, _, _, _, _, _, _}=addr, mask}), do: new(addr, mask, :inet6)
   def new(cidr) when is_binary(cidr) or is_list(cidr) do
     {type, addr, mask} = parse(cidr)
     new(addr, mask, type)
@@ -35,6 +55,7 @@ defmodule Mg.Net.Pool do
   * policy: :low | :high | :random
   * mask: :unique | integer()
   """
+  @spec lease(Pool.t, [policy: policy, mask: mask]) :: {Pool.t, nil | {:inet.ip_address, number}}
   def lease(pool, opts \\ []) do
     block_mask = get_block_mask(pool, Keyword.get(opts, :mask, :unique))
     policy = Keyword.get(opts, :policy, :low)
@@ -42,34 +63,67 @@ defmodule Mg.Net.Pool do
   end
 
   @doc """
-  Return /32 (IPv4) or /128 (IPv6) leases
+  Return leases
   """
-  def leases(p), do: leases(p, [])
+  @spec leases(Pool.t) :: [ {:inet.ip_address, number} ]
+  def leases(p), do: by_status(p, :lease, [])
+
+  @doc """
+  Return reserved blocks
+  """
+  @spec reserved(Pool.t) :: [ {:inet.ip_address, number} ]
+  def reserved(p), do: by_status(p, :reserved, [])
 
   ###
   ### Private
   ###
   defp new(addr, mask, type) do
-    rest_size = bit_size(addr) - mask
-    << addr :: size(mask), _ :: bits >> = addr
+    bin = addr_to_binary(addr)
+    rest_size = bit_size(bin) - mask
+    << addr :: size(mask), _ :: bits >> = bin
     %Pool{ ip: << addr :: size(mask), 0 :: size(rest_size) >>, mask: mask, type: type, netmask: mask }
   end
 
-  defp leases(nil, acc), do: acc
-  defp leases(%Pool{ ip: ip, mask: mask, free: false }, acc) when bit_size(ip) == mask do
-    {addr, _} = to_cidr(ip, mask)
-    [ addr | acc ]
-  end
-  defp leases(%Pool{ low: low, high: high }, acc) do
-    leases(low) ++ leases(high) ++ acc
+  defp parse(cidr) do
+    [addr, mask] = String.split(cidr, "/")
+    {:ok, addr} = :inet.parse_address('#{addr}')
+    {mask, ""} = Integer.parse(mask)
+    type = case tuple_size(addr) do
+             4 -> :inet
+             8 -> :inet6
+           end
+    {type, addr, mask}
   end
 
-  defp do_lease(%Pool{ ip: ip, mask: mask, free: true }=p, mask, _policy) do
-    # This block is available or reserved
-    p = %{ p | free: false }
-    if reserved?(p), do: {p, nil}, else: {p, to_cidr(ip, mask)}
+  defp addr_to_binary(addr) do
+    size = tuple_size(addr) - 1
+    b_size = tuple_size(addr) * 2
+    0..size |> Enum.reduce(<<>>, fn (i, acc) ->
+      acc <> << elem(addr, i) :: size(b_size) >>
+    end)
   end
-  defp do_lease(%Pool{ free: false }=p, _mask, _policy) do
+
+  defp by_status(nil, _, acc), do: acc
+  defp by_status(%Pool{ ip: ip, mask: mask, status: status }, status, acc) do
+    [ to_cidr(ip, mask) | acc ]
+  end
+  defp by_status(%Pool{}=p, status, acc) do
+    by_status(low(p), status, acc) ++ by_status(high(p), status, acc) ++ acc
+  end
+
+  defp do_lease(%Pool{ mask: mask, status: :partial }=p, mask, _policy) do
+    # Block has been partially reserved, can not find sub-block
+    {p, nil}
+  end
+  defp do_lease(%Pool{ ip: ip, mask: mask, status: :free }=p, mask, _policy) do
+    # This block is available or reserved
+    if reserved?(p) do
+      {%{ p | status: :reserved }, nil}
+    else
+      {%{ p | status: :lease }, to_cidr(ip, mask)}
+    end
+  end
+  defp do_lease(%Pool{ status: status }=p, _mask, _policy) when status == :reserved or status == :lease do
     # No available sub-block in this block
     {p, nil}
   end
@@ -92,8 +146,8 @@ defmodule Mg.Net.Pool do
     end
   end
 
-  defp sub_block(%Pool{ low: low, high: _ }=p,    :low), do: low || new_sub_block(p, 0)
-  defp sub_block(%Pool{ low: _,   high: high }=p, :high), do: high || new_sub_block(p, 1)
+  defp sub_block(p, :low),  do: low(p)  || new_sub_block(p, 0)
+  defp sub_block(p, :high), do: high(p) || new_sub_block(p, 1)
 
   # Returns a block with netmask increased by one
   defp new_sub_block(%Pool{ ip: ip, mask: mask, netmask: netmask }, next_bit) do
@@ -106,11 +160,11 @@ defmodule Mg.Net.Pool do
     }
   end
 
-  defp update_pool(pool, block, :low),  do: update_free(%{ pool | low: block })
-  defp update_pool(pool, block, :high), do: update_free(%{ pool | high: block })
+  defp update_pool(p, block, :low), do: update_status(%Pool{ p | low: block })
+  defp update_pool(p, block, :high), do: update_status(%Pool{ p | high: block })
 
-  defp update_free(%Pool{ low: %Pool{ free: false }, high: %Pool{ free: false } }=p), do: %{ p | free: false }
-  defp update_free(p), do: %{ p | free: true }
+  defp update_status(%Pool{ low: nil, high: nil }=p), do: %Pool{ p | status: :free }
+  defp update_status(%Pool{}=p), do: %Pool{ p | status: :partial }
 
   defp to_cidr(b, mask) do
     zero_size = bit_size(b) - mask
@@ -123,25 +177,6 @@ defmodule Mg.Net.Pool do
   defp to_cidr(ip, mask, unit_size, acc) do
     << h :: size(unit_size), rest :: binary >> = ip
     to_cidr(rest, mask, unit_size, [ h | acc])
-  end
-
-  defp parse(cidr) do
-    [addr, mask] = String.split(cidr, "/")
-    {:ok, addr} = :inet.parse_address('#{addr}')
-    {mask, ""} = Integer.parse(mask)
-    type = case tuple_size(addr) do
-             4 -> :inet
-             8 -> :inet6
-           end
-    {type, addr_to_binary(addr), mask}
-  end
-
-  defp addr_to_binary(addr) do
-    size = tuple_size(addr) - 1
-    b_size = tuple_size(addr) * 2
-    0..size |> Enum.reduce(<<>>, fn (i, acc) ->
-      acc <> << elem(addr, i) :: size(b_size) >>
-    end)
   end
 
   defp get_block_mask(%Pool{ type: :inet }, :unique), do: 32
@@ -163,16 +198,19 @@ defmodule Mg.Net.Pool do
     mask == bit_size(ip) and (netaddr(pool) == ip or broadcast(pool) == ip)
   end
 
-  def netaddr(%Pool{ ip: ip, netmask: netmask }) do
+  defp netaddr(%Pool{ ip: ip, netmask: netmask }) do
     rest_size = bit_size(ip) - netmask
     << netaddr :: size(netmask), _ :: size(rest_size) >> = ip
     << netaddr :: size(netmask), 0 :: size(rest_size) >>
   end
 
-  def broadcast(%Pool{ ip: ip, netmask: netmask }) do
+  defp broadcast(%Pool{ ip: ip, netmask: netmask }) do
     rest_size = bit_size(ip) - netmask
     << netaddr :: size(netmask), _ :: size(rest_size) >> = ip
     rest = 1..rest_size |> Enum.reduce(0, fn _, acc -> (acc <<< 1) + 1 end)
     << netaddr :: size(netmask), rest :: size(rest_size) >>
   end
+
+  defp low(%Pool{ low: low }), do: low
+  defp high(%Pool{ high: high }), do: high
 end
