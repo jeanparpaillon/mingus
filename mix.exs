ebec505c90132d4d0a391174876249026c56cef1
Jean Parpaillon <jean.parpaillon@free.fr>
2017-09-22 16:45:40 +0200

    Relax elixir version req

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mg.Mixfile do
     [
       app: :mingus,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: ">= 1.3.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
