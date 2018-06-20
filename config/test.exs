10d737e88d7078a183afdea1a8e8304d7c987872
Jean Parpaillon <jean.parpaillon@free.fr>
2018-06-20 15:10:25 +0200

    Add test env config

---
+++ config/test.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+config :mingus, ssh: [
+  gen_host_key: [ :ed25519, :rsa ],
+  listen: [ {"0.0.0.0", 10022} ]
+]
+
+config :mingus, dns: [
+  listen: [
+    {:udp, "0.0.0.0", 10053},
+    {:tcp, "0.0.0.0", 10053}
+  ]
+]
