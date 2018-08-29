fc548473541756417cc24362b92575e6c085485d
Jean Parpaillon <jean.parpaillon@free.fr>
2018-08-29 11:24:42 +0200

    Setup conf for prod

--- config/prod.exs
+++ config/prod.exs
@@ -6,15 +6,4 @@ config :mingus,
     listen: [{"0.0.0.0", 10022}]
   ]
 
-config :erldns, zone_file: "priv/dns_zones.json"
-
-config :erldns,
-  servers: [
-    [
-      name: :dns_inet11,
-      address: '0.0.0.0',
-      port: 10053,
-      family: :inet,
-      processes: 2
-    ]
-  ]
+import_config "#{Mix.env()}.secret.exs"
